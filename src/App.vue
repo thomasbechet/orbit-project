5ad8aa5da44c1e510aef27504891416c352c6293
Thomas <thomas.bechet.pro@gmail.com>
2021-02-20 12:07:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Viewport/>
+  </div>
+</template>
+
+<script>
+import Viewport from './components/Viewport.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Viewport
+  }
+}
+</script>
+
+<style>
+</style>
